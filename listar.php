9dddd4ca857506bd92f73cdd0707659030482f8a
Gabs <130662762+gabzin16@users.noreply.github.com>
2024-02-08 08:03:42 -0300

    Iniciando

---
+++ listar.php
@@ -0,0 +1,10 @@
+<!-- Digite aqui (listar.php) -->
+
+<!-- 1º Arquivo a ser digitado -->
+
+<?php 
+
+// Inclui conexão com o banco de dados
+include_once "conexao.php";
+
+// Receber a página atual método GET
